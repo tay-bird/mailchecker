04a606d6552e22a446dc63bd4a99dd90b05481b8
Tay Frost <taylor@taybird.com>
2016-07-17 18:36:54 -0700

    Add setup.py

---
+++ mailchecker/__init__.py
@@ -0,0 +1,1 @@
+from mailchecker import EmailChecker
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='mailchecker',
+      version='0.1',
+      author='Tay Frost',
+      author_email='tay@taybird.com',
+      url='https://github.com/tay-bird/mailchecker/',
+     )
